c54a9f2326432e67ebe68faf2f6ae2ba2bbe0aee
Ryan Chandler <ryangjchandler@gmail.com>
2021-06-22 11:04:45 +0100

    chore: meta tagS

---
+++ source/_layouts/meta.blade.php
@@ -0,0 +1,8 @@
+<meta property="og:title" content="{{ $page->title }}" />
+<meta property="og:description" content="A small collection of free Tailwind components." />
+<meta name="description" content="A small collection of free Tailwind components." />
+<meta property="og:type" content="website" />
+<meta property="og:site_name" content="Tinywind.dev">
+<meta property="og:image" content="assets/images/og.png" />
+<meta property="og:url" content="{{ $page->getUrl() }}" />
+<link rel="canonical" href="{{ $page->getUrl() }}" />
