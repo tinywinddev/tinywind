afb5f1c0045c2cc70d671c215502282f56710675
Ryan Chandler <ryangjchandler@gmail.com>
2021-06-22 11:12:56 +0100

    fix: og images

--- source/_layouts/meta.blade.php
+++ source/_layouts/meta.blade.php
@@ -3,6 +3,6 @@
 <meta name="description" content="A small collection of free Tailwind components." />
 <meta property="og:type" content="website" />
 <meta property="og:site_name" content="Tinywind.dev">
-<meta property="og:image" content="assets/images/og.png" />
+<meta property="og:image" content="https://tinywind.dev/assets/images/og.png" />
 <meta property="og:url" content="{{ $page->getUrl() }}" />
 <link rel="canonical" href="{{ $page->getUrl() }}" />
