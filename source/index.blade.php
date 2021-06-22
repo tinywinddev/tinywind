9db080b362d9209cdee6b2652a5e49d07e21db14
Ryan Chandler <ryangjchandler@gmail.com>
2021-06-22 13:33:38 +0100

    feature: component requests

--- source/index.blade.php
+++ source/index.blade.php
@@ -26,5 +26,13 @@
                 </svg>
             </div>
         </a>
+        <a href="https://github.com/ryangjchandler/tinywind/issues/new?assignees=ryangjchandler&labels=component+request&template=component-request.md&title=" target="_blank" class="block">
+            <div class="flex items-center justify-between px-6 py-4 border border-gray-300 rounded-md hover:border-gray-500">
+                <h3 class="text-lg font-medium">Request a component</h3>
+                <svg width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M10.75 8.75L14.25 12L10.75 15.25"></path>
+                </svg>
+            </div>
+        </a>
     </section>
 @endsection
