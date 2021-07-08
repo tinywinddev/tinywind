5bd53d2fcd52e96d1a1f838774259aca01d70c0d
Ryan Chandler <ryangjchandler@gmail.com>
2021-07-08 18:17:32 +0100

    wip

--- source/_components/dividers/badge.blade.php
+++ source/_components/dividers/badge.blade.php
@@ -3,7 +3,7 @@
         aria-hidden="true"
         class="flex-grow bg-gray-200 rounded h-0.5"
     ></span>
-    <span class="inline-block px-4 py-1 text-sm font-semibold text-center text-gray-500 bg-gray-300 rounded-full">
+    <span class="inline-block px-4 py-1 text-sm font-medium text-center text-gray-500 bg-gray-200 rounded-full">
         Badge
     </span>
     <span
--- source/_components/dividers/button.blade.php
+++ source/_components/dividers/button.blade.php
@@ -3,7 +3,7 @@
         aria-hidden="true"
         class="flex-grow bg-gray-200 rounded h-0.5"
     ></span>
-    <button type="button" class="px-4 py-1 text-sm font-medium text-center text-gray-900 bg-white border border-gray-200 rounded-md shadow-sm hover:bg-gray-100 focus:outline-black">
+    <button type="button" class="px-4 py-1 text-sm font-medium text-center text-gray-900 bg-white border border-gray-200 rounded-md shadow-sm hover:bg-gray-50 focus:outline-black">
         Click me
     </button>
     <span
